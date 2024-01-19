dd60e78061ce24afd6940a0c74fda0180f02a537
Maria <mariaizgar01@gmail.com>
2024-01-19 18:00:25 +0100

    Images changes

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const element2 = document.getElementById('my-work-section')
 
 if (element) {
   element.addEventListener('click', () => {
-    element2.scrollIntoView({behavior: "smooth", block: "start"});
+    element2.scrollIntoView({ behavior: "smooth", block: "start" });
   })
 }
 
