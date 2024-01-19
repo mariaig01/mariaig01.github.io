3f4e677fc7824c4f125089aa597d954aec8853d3
Maria <mariaizgar01@gmail.com>
2024-01-19 17:46:09 +0100

    minor changes in scroll animation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
+const element = document.getElementById('my-work-link')
+const element2 = document.getElementById('my-work-section')
+
+if (element) {
+  element.addEventListener('click', () => {
+    element2.scrollIntoView({behavior: "smooth", block: "start"});
+  })
+}
+
+/*
 if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-link').addEventListener('click', () => {
       document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
     })
-  }
+  }*/
