8e76aeb64467f61b5a95c0a7e27f60d47f71cfd3
Maria <mariaizgar01@gmail.com>
2024-01-20 15:56:12 +0100

    minor changes tooltip display position

--- js/script.js
+++ js/script.js
@@ -7,9 +7,19 @@ if (element) {
   })
 }
 
-/*
-if (document.getElementById('my-work-link')) {
-    document.getElementById('my-work-link').addEventListener('click', () => {
-      document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
-    })
-  }*/
+function adjustTooltipPosition(event) {
+  const tooltip = event.target.nextElementSibling;
+  const imgRect = event.target.getBoundingClientRect();
+  const windowWidth = window.innerWidth;
+
+  const tooltipWidth = tooltip.offsetWidth;
+  const leftOffset = imgRect.left + imgRect.width / 2 - tooltipWidth / 2;
+
+  if (leftOffset < 0) {
+      tooltip.style.left = '0';
+  } else if (leftOffset + tooltipWidth > windowWidth) {
+      tooltip.style.left = windowWidth - tooltipWidth + 'px';
+  } else {
+      tooltip.style.left = leftOffset + 'px';
+  }
+}
